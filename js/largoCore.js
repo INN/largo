e0872c75793cfc65c467c218c7188c7e52dcb816
Ben Keith <ben@benlk.com>
2015-05-14 01:22:05 -0400

    Removing current-specific newsletter signup form code

--- js/largoCore.js
+++ js/largoCore.js
@@ -425,15 +425,4 @@ jQuery(document).ready(function($) {
 	}
 	$(window).resize(navOverflow);
 	$(window).scroll(navOverflow).scroll();
-
-	// Newsletter signup form interaction
-	$('#site-header .newsletter-signup .email_address').focus(function() {
-		$(this).siblings('.first_name, .last_name, .submit').show();
-	});
-
-	$(document).mouseup(function(e) {
-		var container = $("#site-header .newsletter-signup");
-		if (!container.is(e.target) && container.has(e.target).length === 0)
-			container.find('.first_name, .last_name, .submit, .error').hide();
-	});
 });
