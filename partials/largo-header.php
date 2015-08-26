41d2296c2de506f94529355f4d99d8b1b9a8df4f
David Ryan <david@inn.org>
2015-08-26 12:02:37 -0700

    sticky nav conditional for aspen

--- partials/largo-header.php
+++ partials/largo-header.php
@@ -1,6 +1,8 @@
+<?php if( SHOW_STICKY_NAV === FALSE && is_front_page() || is_home() ) { ?>
 <header id="site-header" class="clearfix nocontent" itemscope itemtype="http://schema.org/Organization">
 	<?php largo_header(); ?>
 </header>
+<?php } ?>
 <header class="print-header nocontent">
 	<p><strong><?php echo esc_html( get_bloginfo( 'name' ) ); ?></strong> (<?php echo esc_url( largo_get_current_url() ); ?>)</p>
 </header>
