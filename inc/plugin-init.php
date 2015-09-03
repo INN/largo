fbae5a9dce646ed719dc2e872f9a8aa947c2b033
Jason Lemieux <jason@gopostmatic.com>
2015-09-03 00:35:53 +0000

    added Epoch to the installer. nice.

--- inc/plugin-init.php
+++ inc/plugin-init.php
@@ -68,8 +68,8 @@ function largo_register_required_plugins() {
 
 		// This is an example of how to include a plugin from the WordPress Plugin Repository
 		array(
-			'name' 		=> 'Epoch Commenting',
-			'slug' 		=> 'epoch',
+			'name' 		=> 'Epoch',
+			'slug' 		=> 'Epoch',
 			'required' 	=> false,
 		),
 
