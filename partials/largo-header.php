3f4ce129216351938f5ff690956f67be6205bec9
David Ryan <david@inn.org>
2015-09-17 20:02:20 -0700

    docblocks

--- partials/largo-header.php
+++ partials/largo-header.php
@@ -1,6 +1,28 @@
+<?php
+/**
+ * Largo Header
+ *
+ * Simple markup to generate the theme header and fallback header for printing pages.
+ *
+ * @package Largo
+ */ ?>
 <header id="site-header" class="clearfix nocontent" itemscope itemtype="http://schema.org/Organization">
-	<?php largo_header(); ?>
+	<?php 
+	/**
+     * Largo Header Output
+     *
+     * Found in the inc/header-footer.php partial, largo_header() is pluggable in Child
+     * Themes as Largo checks if it exists elsewhere first.
+     *
+     * @see inc/header-footer.php
+     * @package Largo
+     */
+	 largo_header(); ?>
 </header>
+
+<!-- BEGIN Print-Only Header -->
 <header class="print-header nocontent">
-	<p><strong><?php echo esc_html( get_bloginfo( 'name' ) ); ?></strong> (<?php echo esc_url( largo_get_current_url() ); ?>)</p>
+	<p>
+	  <strong><?php echo esc_html( get_bloginfo( 'name' ) ); ?></strong> (<?php echo esc_url( largo_get_current_url() ); ?>)</p>
 </header>
+<!-- END Print-Only Header -->
